6abc8bc8368580fc4c07b1f21bd989cad6b7afab
Liubov K <lyouso@yandex.ru>
2022-04-24 13:26:11 +0300

    Init commit

---
+++ Task 66/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+int m = 1;
+int n = 15;
+int sum = 0;
+Console.WriteLine(Recursion(m, n));
+
+int Recursion(int m, int n)
+{
+    if (n + 1 == m) return 0;
+    sum += m;
+    Recursion(m + 1, n);
+    return sum;
+}
---
+++ Task 68/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(n,m) = 29 */
+
+int a = 3;
+int b = 2;
+Console.Write("Значение функции Аккермана равно " + Aсkermann(a, b));
+
+int Aсkermann(int m, int n)
+{
+    if (m==0) return n+1;
+    else if (n == 0) return Aсkermann(m - 1, 1);
+    else return Aсkermann(m - 1, Aсkermann(m, n-1));
+}
